bc26bda82fedd55f1ce3226fee716512832f7492
Abdullateef Abiodun <yomlateef@yahoo.com>
2023-02-01 12:45:26 +0100

    fix: set proper port and host

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const cors_proxy = require("cors-anywhere");
 
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || "0.0.0.0";
+
 cors_proxy
   .createServer({
     originWhitelist: [], // Allow all origins
     // requireHeader: ['origin', 'x-requested-with'],
     // removeHeaders: ['cookie', 'cookie2']
   })
-  .listen(8080, "0.0.0.0", () => {
-    console.log(`Running CORS anywhere on 0.0.0.0:8080`);
+  .listen(PORT, HOST, () => {
+    console.log(`Running CORS anywhere on ${HOST}:${PORT}`);
   });
